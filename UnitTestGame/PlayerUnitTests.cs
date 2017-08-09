d9f95e1544f2b794de6d67327aa083915ba4c5e3
twizzleness <tylerforaie@gmail.com>
2017-08-08 22:51:44 -0600

    Worked on unit testing

--- UnitTestGame/PlayerUnitTests.cs
+++ UnitTestGame/PlayerUnitTests.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Game;
 using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
 
 namespace UnitTestGame
 {
@@ -11,9 +13,22 @@ namespace UnitTestGame
         [TestMethod]
         public void TestPlayerArmyDecleration()
         {
+            var army = new List<IUnit>() { new Barbarian(25, 10), new Archer(25, 10), new Wizard(25, 10) };
             var player = new Player("Name", new Army(army));
 
+<<<<<<< HEAD:UnitTestGame/PlayerUnitTests.cs
             Assert.AreEqual(unit.GetType(), player.Army.GetFirstUnit().GetType());
+=======
+
+            Assert.AreEqual(army.ElementAt(0).GetType(), player.Army.Units.ElementAt(0).GetType());
+            System.Console.WriteLine($"Expected: {army.ElementAt(0).GetType()} Actual: {player.Army.Units.ElementAt(0).GetType()}");
+            Assert.AreEqual(army.ElementAt(1).GetType(), player.Army.Units.ElementAt(1).GetType());
+            System.Console.WriteLine($"Expected: {army.ElementAt(1).GetType()} Actual: {player.Army.Units.ElementAt(1).GetType()}");
+            Assert.AreEqual(army.ElementAt(2).GetType(), player.Army.Units.ElementAt(2).GetType());
+            System.Console.WriteLine($"Expected: {army.ElementAt(2).GetType()} Actual: {player.Army.Units.ElementAt(2).GetType()}");
+
+
+>>>>>>> TestBranch:UnitTestGame/UnitTest1.cs
         }
     }
 }
